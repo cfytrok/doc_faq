04f25590981981186108834c5966b1b4bc477ae3
Aleksandr Gavrilov <agvrdir@gmail.com>
2024-07-06 13:33:13 +0300

    update requirements for steamlit deployment

--- retriever.py
+++ retriever.py
@@ -3,6 +3,11 @@ from pathlib import Path
 import streamlit as st
 from git import Repo
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
